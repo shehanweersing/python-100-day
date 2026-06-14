fc7372bdce2940dc8bce731941a1a174fb50eb43
shehanweersing <shehanweersing@gmail.com>
2026-06-14 08:17:22 +0530

    random head or tail

--- Random module.py
+++ Random module.py
@@ -5,3 +5,14 @@ random_number = random.randint(1,12)
 random_float = random.random()
 print(random_number)
 print(random_float)
+
+
+
+random_head_or_tail = random.randint(0,1)
+
+print(random_head_or_tail)
+if random_head_or_tail == 1:
+    print("Head")
+else:
+    print("Tail")
+        
