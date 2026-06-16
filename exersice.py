876e49123c3eb9d6720826a4d8e7384b5f35b146
shehanweersing <shehanweersing@gmail.com>
2026-06-16 11:02:46 +0530

    12

--- exersice.py
+++ exersice.py
@@ -1,8 +1,19 @@
+import random
+
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+#nr_symbols = int(input(f"How many symbols would you like?\n"))
+#nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_letters =""
+for _ in range(nr_letters):
+
+random_letters += random.choice(letters)
+
+print(random_letters)
+#print(symbols)
