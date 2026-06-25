0754decb284a358895cfcb409d52a890ef16231a
shehanweersing <shehanweersing@gmail.com>
2026-06-25 09:37:05 +0530

    5

--- def.py
+++ def.py
@@ -34,3 +34,27 @@ turn_right()
 move()
 move()
 turn_around()
+
+
+####https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+###try bellow commands on this web page
+
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+    
+    
+for i in range(6):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
