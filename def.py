37abccd425a055ad8d67c8c30548984da1453c6a
shehanweersing <shehanweersing@gmail.com>
2026-06-25 09:21:15 +0530

    3

--- def.py
+++ def.py
@@ -5,3 +5,17 @@ def my_function() :
 
 
 my_function()    
+
+
+#turn_left()  turning to left
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_around()
+turn_right()
