5c0fc81576841993951fa06c0ead22473d4306f6
shehanweersing <shehanweersing@gmail.com>
2026-06-26 10:08:24 +0530

    1

--- def.py
+++ def.py
@@ -58,3 +58,34 @@ for i in range(6):
     turn_right()
     move()
     turn_left()
+
+
+
+
+            ######## https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json   
+
+  
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+while not at_goal():
+    if wall_in_front():
+       jump()
+    else:
+        move()
+
+    
+          
